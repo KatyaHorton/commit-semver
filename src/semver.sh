f28cf2edb46ed3a21141bcb3327b7480c11b071c
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 12:54:39 +0800

    test no feat

--- src/semver.sh
+++ src/semver.sh
@@ -2,6 +2,10 @@
 
 
 COMMIT_MESSAGE = ${git log --pretty='format:%Creset%s' --no-merges -1}
-# read -p 'who is it? ' gitcommit
-
+if [COMMIT_MESSAGE == 'feat'] 
+then
+    npm version patch
+    git add . 
+    git commit -m COMMIT_MESSAGE
+fi
 echo "Message was: " $COMMIT_MESSAGE
