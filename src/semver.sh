0c07202567459c79c05aa14f82eae8977f45fb26
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 12:56:23 +0800

    test no feat1

--- src/semver.sh
+++ src/semver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-COMMIT_MESSAGE = ${git log --pretty='format:%Creset%s' --no-merges -1}
-if [COMMIT_MESSAGE == 'feat'] 
-then
-    npm version patch
-    git add . 
-    git commit -m COMMIT_MESSAGE
-fi
-echo "Message was: " $COMMIT_MESSAGE
+echo "Message was: " git log --pretty='format:%Creset%s' --no-merges -1
+# if [COMMIT_MESSAGE == 'feat'] 
+# then
+#     npm version patch
+#     git add . 
+#     git commit -m COMMIT_MESSAGE
+# fi
+# echo "Message was: " ${git log --pretty='format:%Creset%s' --no-merges -1}
