3ac8bcc31943620e84f955396e57e7515a3a697d
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 13:29:19 +0800

    test

--- src/semver.sh
+++ src/semver.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
   
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+
+if[ "$MESSAGE" == 'feat' ]
+    then 
+        echo "Is feat"
+    else 
+        echo "IS NOT"
+fi
+
 echo "START: $MESSAGE END"
 
 
