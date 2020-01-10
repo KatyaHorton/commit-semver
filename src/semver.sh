2018f06d9bc7a6b6af5faad5b6cb666fe1b6b6e2
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 12:58:25 +0800

    test no feat11

--- src/semver.sh
+++ src/semver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Message was: " git log --pretty='format:%Creset%s' --no-merges -1
+echo "Message was: " ${git log --format=%B -n 1 <commit>}
 # if [COMMIT_MESSAGE == 'feat'] 
 # then
 #     npm version patch
