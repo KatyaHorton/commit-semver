abe8c8fd0521aaec377803e3333298e9d5bd1ac5
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 13:03:10 +0800

    test again

--- src/semver.sh
+++ src/semver.sh
@@ -3,10 +3,11 @@
 
 message=$(git log --format=%B -n 1 <commit>)
 echo "$messsage"
-# if [COMMIT_MESSAGE == 'feat'] 
-# then
-#     npm version patch
-#     git add . 
-#     git commit -m COMMIT_MESSAGE
-# fi
+if ["$messsage" != 'feat'] 
+then
+echo "Not equal"
+    # npm version patch
+    # git add . 
+    # git commit -m COMMIT_MESSAGE
+fi
 # echo "Message was: " ${git log --pretty='format:%Creset%s' --no-merges -1}
