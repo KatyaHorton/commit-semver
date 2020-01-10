f8457b2eb637a98c4691ed352273e8ff0873bf50
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 17:08:38 +0800

    another commit message

--- src/semver.sh
+++ src/semver.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-  
 
-MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
-if [ "$MESSAGE" == 'feat' ]
-    then 
-        echo "Is feat $MESSAGE"
-    else 
-        echo "$MESSAGE IS NOT feat"
-fi
-echo "START: $MESSAGE END"
+MESSAGE=$(git log origin/master..master --pretty=format:%s)
+
+# if [ "$MESSAGE" == 'feat' ]
+#     then 
+#         echo "Is feat $MESSAGE"
+#     else 
+#         echo "$MESSAGE IS NOT feat"
+# fi
+echo "$MESSAGE"
 
 
 
