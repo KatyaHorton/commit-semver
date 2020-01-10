ed0bf64d191661b3781689b11703b654ad880d34
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-10 13:38:58 +0800

    smth

--- src/semver.sh
+++ src/semver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
   
-MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+MESSAGE=$(git diff --cached)
+# MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
 if [ "$MESSAGE" == 'feat' ]
     then 
