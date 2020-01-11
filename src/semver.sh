bbfc9c0a73f9fcd5d8e7a3833c51b240d9325c7a
Mark Horton <markhorton@Marks-MacBook-Pro.local>
2020-01-11 14:20:01 +0800

    some trandome commit

--- src/semver.sh
+++ src/semver.sh
@@ -15,12 +15,12 @@ do
 echo ${names[$i]}
   COMMIT_MESSAGE=${names[$i]}
     if [ "${COMMIT_MESSAGE}" == *'BREAKING CHANGES'* ];
-        then  $(npm version major)
+        then  npm version major  
     elif [ "${COMMIT_MESSAGE}" == *'feat'* ];
         then  npm version minor
     elif [ "${COMMIT_MESSAGE}" == *'fix'* ];
         then  npm version patch
  fi
 
-   npm version major  
+   
 done
